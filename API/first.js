4491c7b94fc8036cfe0fce3bc5623862e220aed7
Sahil4430 <panwarsahil204@gmail.com>
2025-07-30 22:58:42 +0530

    post

--- API/first.js
+++ API/first.js
@@ -3,7 +3,7 @@
  const app = express();
 const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`));
 
-
+ app.use(express.json());  // this ius use as a midlw ware to parse JSON bodies
   app.get('/api/v1/destinations',(req,res)=>{
     res.status(200).json({
         status: 'success',
@@ -13,6 +13,20 @@ const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`)
         }
   });
 });
+ app.post('/api/v1/destinations',(req,res)=>{
+  const NEW_ID = dest[dest.length -1].id + 1;
+  const ne_dest = Object.assign({id: NEW_ID}, req.body); // this will create a new object with the new id and the body of the request
+  //  const ne_dest = req.body;
+   dest.push(ne_dest);
+   fs.writeFile(`${__dirname}/../data/destination.json`, JSON.stringify(dest), err =>{
+    res.status(201).json({
+      status:'success',
+      data: {
+        dest: ne_dest
+      }
+    })
+   })
+ })
 
 
   const port  = 3000;
