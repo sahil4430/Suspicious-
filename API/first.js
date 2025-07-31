85f2e60fa1b3e8c16d8351957761fa270da6f14e
Sahil4430 <panwarsahil204@gmail.com>
2025-07-31 20:40:58 +0530

    Refactor routes

--- API/first.js
+++ API/first.js
@@ -4,7 +4,8 @@
 const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`));
 
  app.use(express.json());  // this ius use as a midlw ware to parse JSON bodies
-  app.get('/api/v1/destinations',(req,res)=>{
+
+ const Getalldest= (req,res)=>{
     res.status(200).json({
         status: 'success',
         result:dest.length,
@@ -12,8 +13,8 @@ const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`)
            dest
         }
   });
-});
-  app.get('/api/v1/destinations/:id',(req,res)=>{
+};
+const GetDest= (req,res)=>{
     
     const ID = req.params.id * 1;  // it convert any number into int by multiplying by 1
      if(ID > dest.length){
@@ -30,8 +31,8 @@ const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`)
            ndest
         }
   });
-});
- app.post('/api/v1/destinations',(req,res)=>{
+}; 
+ const POSTDest = (req,res)=>{
   const NEW_ID = dest[dest.length -1].id + 1;
   const ne_dest = Object.assign({id: NEW_ID}, req.body); // this will create a new object with the new id and the body of the request
   //  const ne_dest = req.body;
@@ -44,17 +45,18 @@ const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`)
       }
     })
    })
- })
- app.patch('/api/v1/destinations/:id',(req,res)=>{
+ };
+
+ const UpdateDest = (req,res)=>{
   res.status(200).json({
     status: 'success',
     data: {
       dest: '<updated destination here>'
     }
   })
- })
+ };
 
- app.delete('/api/v1/destinations/:id',(req,res)=>{
+ const DeleteDest = (req,res)=>{
    if ( req.params.id*1 > dest.length){
     return res.status(404).json({
       status:'fail',
@@ -66,8 +68,20 @@ const dest = JSON.parse(fs.readFileSync(`${__dirname}/../data/destination.json`)
     status: 'success',
     data:null,
    })
- })
+ };
 
+ // routes. 
+
+ //traditional way
+  // app.get('/api/v1/destinations',Getalldest);
+//   app.get('/api/v1/destinations/:id', GetDest);
+//  app.post('/api/v1/destinations', POSTDest)
+//  app.patch('/api/v1/destinations/:id', UpdateDest)
+//  app.delete('/api/v1/destinations/:id', DeleteDest)
+
+ app.route('/api/v1/destinations').get(Getalldest).post(POSTDest);
+ app.route ('/api/v1/destinations/:id').get(GetDest).patch(UpdateDest).delete(DeleteDest);
+  // this is the server
   const port  = 3000;
   app.listen (port,()=>{
     console.log(`server is running on port ${port}`);
