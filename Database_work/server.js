e685e56801e391e4888e809e02f42b494a39835d
Sahil4430 <panwarsahil204@gmail.com>
2025-08-28 20:26:56 +0530

    done making schema and model

--- Database_work/server.js
+++ Database_work/server.js
@@ -9,3 +9,14 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 mongoose.connect(DB).then(()=>console.log('DB connection successful'));
+
+const tour_Schema = new mongoose.Schema({
+    name: { type: String, required: [true, 'tour must contain a name '] , unique:true},
+    rating : {type: Number, default: 4.5},
+    price:{
+        type : Number,
+        required:[true,"tourn must contain a price"]
+    }
+})
+
+const Tour = mongoose.model('Tour', tour_Schema)
